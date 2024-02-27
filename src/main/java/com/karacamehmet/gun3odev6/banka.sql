374742594a3f4f223d2b3f85c36debae1fbf03e3
Mehmet Karaca <114088915+karaca12@users.noreply.github.com>
2024-02-28 01:28:33 +0300

    Update banka.sql

--- src/main/java/com/karacamehmet/gun3odev6/banka.sql
+++ src/main/java/com/karacamehmet/gun3odev6/banka.sql
@@ -1,123 +1,149 @@
--- MySQL Workbench Synchronization
--- Generated: 2024-02-28 01:15
--- Model: New Model
--- Version: 1.0
--- Project: Name of the project
--- Author: memo_
+-- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-ALTER SCHEMA `banka`  DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci ;
+-- -----------------------------------------------------
+-- Schema Banka
+-- -----------------------------------------------------
 
-ALTER TABLE `banka`.`Musteriler` 
-DROP FOREIGN KEY `idKanallar`;
+-- -----------------------------------------------------
+-- Schema Banka
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Banka` DEFAULT CHARACTER SET utf8 ;
+USE `Banka` ;
 
-ALTER TABLE `banka`.`GercekMusteriler` 
-DROP FOREIGN KEY `idMusteriGercek`;
+-- -----------------------------------------------------
+-- Table `Banka`.`Kanallar`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Banka`.`Kanallar` (
+  `idKanallar` INT NOT NULL,
+  `Ad` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idKanallar`))
+ENGINE = InnoDB;
 
-ALTER TABLE `banka`.`TuzelMusteriler` 
-DROP FOREIGN KEY `idMusteriTuzel`;
 
-ALTER TABLE `banka`.`Adresler` 
-DROP FOREIGN KEY `idMusteriAdres`;
+-- -----------------------------------------------------
+-- Table `Banka`.`Musteriler`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Banka`.`Musteriler` (
+  `idMusteri` INT NOT NULL,
+  `MusteriNo` CHAR(10) NOT NULL,
+  `idKanallar` INT NOT NULL,
+  PRIMARY KEY (`idMusteri`),
+  INDEX `KanalId_idx` (`idKanallar` ASC) VISIBLE,
+  UNIQUE INDEX `MusteriNo_UNIQUE` (`MusteriNo` ASC) VISIBLE,
+  CONSTRAINT `idKanallar`
+    FOREIGN KEY (`idKanallar`)
+    REFERENCES `Banka`.`Kanallar` (`idKanallar`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-ALTER TABLE `banka`.`Bilancolar` 
-DROP FOREIGN KEY `idMusteriBilanco`;
 
-ALTER TABLE `banka`.`TicariFaaliyetler` 
-DROP FOREIGN KEY `idMusteriTicari`;
+-- -----------------------------------------------------
+-- Table `Banka`.`GercekMusteriler`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Banka`.`GercekMusteriler` (
+  `idMusteri` INT NOT NULL,
+  `Ad` VARCHAR(45) NOT NULL,
+  `Soyad` VARCHAR(45) NOT NULL,
+  `TCKimlikNo` CHAR(11) NOT NULL,
+  PRIMARY KEY (`idMusteri`),
+  UNIQUE INDEX `TCKimlikNo_UNIQUE` (`TCKimlikNo` ASC) VISIBLE,
+  CONSTRAINT `idMusteriGercek`
+    FOREIGN KEY (`idMusteri`)
+    REFERENCES `Banka`.`Musteriler` (`idMusteri`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-ALTER TABLE `banka`.`Sendikalar` 
-DROP FOREIGN KEY `fk_Sendikalar_Musteriler1`;
 
-ALTER TABLE `banka`.`Musteriler` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-CHANGE COLUMN `idMusteri` `idMusteri` INT(11) NOT NULL ,
-CHANGE COLUMN `idKanallar` `idKanallar` INT(11) NOT NULL ;
+-- -----------------------------------------------------
+-- Table `Banka`.`TuzelMusteriler`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Banka`.`TuzelMusteriler` (
+  `idMusteri` INT NOT NULL,
+  `Unvan` VARCHAR(45) NOT NULL,
+  `VergiNo` CHAR(10) NOT NULL,
+  PRIMARY KEY (`idMusteri`),
+  UNIQUE INDEX `VergiNo_UNIQUE` (`VergiNo` ASC) VISIBLE,
+  CONSTRAINT `idMusteriTuzel`
+    FOREIGN KEY (`idMusteri`)
+    REFERENCES `Banka`.`Musteriler` (`idMusteri`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-ALTER TABLE `banka`.`GercekMusteriler` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-CHANGE COLUMN `idMusteri` `idMusteri` INT(11) NOT NULL ;
 
-ALTER TABLE `banka`.`TuzelMusteriler` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-CHANGE COLUMN `idMusteri` `idMusteri` INT(11) NOT NULL ;
+-- -----------------------------------------------------
+-- Table `Banka`.`Adresler`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Banka`.`Adresler` (
+  `idAdres` INT NOT NULL,
+  `idMusteri` INT NOT NULL,
+  `AdresDetay` VARCHAR(250) NOT NULL,
+  PRIMARY KEY (`idAdres`),
+  INDEX `idMusteri_idx` (`idMusteri` ASC) VISIBLE,
+  CONSTRAINT `idMusteriAdres`
+    FOREIGN KEY (`idMusteri`)
+    REFERENCES `Banka`.`Musteriler` (`idMusteri`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-ALTER TABLE `banka`.`Adresler` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-CHANGE COLUMN `idAdres` `idAdres` INT(11) NOT NULL ,
-CHANGE COLUMN `idMusteri` `idMusteri` INT(11) NOT NULL ;
 
-ALTER TABLE `banka`.`Bilancolar` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-CHANGE COLUMN `idBilancolar` `idBilancolar` INT(11) NOT NULL ,
-CHANGE COLUMN `idMusteri` `idMusteri` INT(11) NOT NULL ,
-CHANGE COLUMN `Tarih` `Tarih` DATETIME NOT NULL ;
+-- -----------------------------------------------------
+-- Table `Banka`.`Bilancolar`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Banka`.`Bilancolar` (
+  `idBilancolar` INT NOT NULL,
+  `idMusteri` INT NOT NULL,
+  `Tarih` DATETIME NOT NULL,
+  `Tutar` FLOAT NOT NULL,
+  PRIMARY KEY (`idBilancolar`),
+  INDEX `idMusteri_idx` (`idMusteri` ASC) VISIBLE,
+  CONSTRAINT `idMusteriBilanco`
+    FOREIGN KEY (`idMusteri`)
+    REFERENCES `Banka`.`Musteriler` (`idMusteri`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-ALTER TABLE `banka`.`TicariFaaliyetler` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-CHANGE COLUMN `idTicariFaaliyetler` `idTicariFaaliyetler` INT(11) NOT NULL ,
-CHANGE COLUMN `idMusteri` `idMusteri` INT(11) NOT NULL ,
-CHANGE COLUMN `BaslangicTarih` `BaslangicTarih` DATETIME NOT NULL ;
 
-ALTER TABLE `banka`.`Kanallar` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-CHANGE COLUMN `idKanallar` `idKanallar` INT(11) NOT NULL ;
+-- -----------------------------------------------------
+-- Table `Banka`.`TicariFaaliyetler`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Banka`.`TicariFaaliyetler` (
+  `idTicariFaaliyetler` INT NOT NULL,
+  `idMusteri` INT NOT NULL,
+  `BaslangicTarih` DATETIME NOT NULL,
+  PRIMARY KEY (`idTicariFaaliyetler`),
+  INDEX `idMusteri_idx` (`idMusteri` ASC) VISIBLE,
+  UNIQUE INDEX `idMusteri_UNIQUE` (`idMusteri` ASC) VISIBLE,
+  CONSTRAINT `idMusteriTicari`
+    FOREIGN KEY (`idMusteri`)
+    REFERENCES `Banka`.`GercekMusteriler` (`idMusteri`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-ALTER TABLE `banka`.`Sendikalar` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-CHANGE COLUMN `idMusteri` `idMusteri` INT(11) NOT NULL ;
 
-ALTER TABLE `banka`.`Musteriler` 
-ADD CONSTRAINT `idKanallar`
-  FOREIGN KEY (`idKanallar`)
-  REFERENCES `banka`.`Kanallar` (`idKanallar`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `banka`.`GercekMusteriler` 
-ADD CONSTRAINT `idMusteriGercek`
-  FOREIGN KEY (`idMusteri`)
-  REFERENCES `banka`.`Musteriler` (`idMusteri`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `banka`.`TuzelMusteriler` 
-ADD CONSTRAINT `idMusteriTuzel`
-  FOREIGN KEY (`idMusteri`)
-  REFERENCES `banka`.`Musteriler` (`idMusteri`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `banka`.`Adresler` 
-ADD CONSTRAINT `idMusteriAdres`
-  FOREIGN KEY (`idMusteri`)
-  REFERENCES `banka`.`Musteriler` (`idMusteri`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `banka`.`Bilancolar` 
-ADD CONSTRAINT `idMusteriBilanco`
-  FOREIGN KEY (`idMusteri`)
-  REFERENCES `banka`.`Musteriler` (`idMusteri`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `banka`.`TicariFaaliyetler` 
-ADD CONSTRAINT `idMusteriTicari`
-  FOREIGN KEY (`idMusteri`)
-  REFERENCES `banka`.`GercekMusteriler` (`idMusteri`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `banka`.`Sendikalar` 
-ADD CONSTRAINT `fk_Sendikalar_Musteriler1`
-  FOREIGN KEY (`idMusteri`)
-  REFERENCES `banka`.`Musteriler` (`idMusteri`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+-- -----------------------------------------------------
+-- Table `Banka`.`Sendikalar`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Banka`.`Sendikalar` (
+  `idMusteri` INT NOT NULL,
+  `Ad` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idMusteri`),
+  INDEX `fk_Sendikalar_Musteriler1_idx` (`idMusteri` ASC) VISIBLE,
+  CONSTRAINT `fk_Sendikalar_Musteriler1`
+    FOREIGN KEY (`idMusteri`)
+    REFERENCES `Banka`.`Musteriler` (`idMusteri`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
